a6e3793fa412104ce59703cf88cd9db8ef223e0a
Lev4and <54065747+Lev4and@users.noreply.github.com>
2023-01-29 12:09:13 +0500

    Created backend project.

---
+++ TicTakToe.WebApplication/Extensions/CorsExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace TicTakToe.WebApplication.Extensions
+{
+    internal static class CorsExtensions
+    {
+        public const string CorsPolicyName = "CorsPolicy";
+
+        private static readonly string[] _origins = new string[2]
+        {
+            "http://localhost", "http://lev4and.ru"
+        };
+
+        public static void AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(setup =>
+            {
+                setup.AddPolicy(CorsPolicyName, policy => policy.WithOrigins(_origins)
+                    .AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+            });
+        }
+
+        public static IApplicationBuilder UseCorsPolicy(this IApplicationBuilder builder)
+        {
+            return builder.UseCors(CorsPolicyName);
+        }
+    }
+}
